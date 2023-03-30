3e7475e5f8a904cfe780f0c52855637decbfd535
Pham Chu Duong <pduong244@gmail.com>
2023-03-30 10:05:40 +0700

    Áp dụng mô hình MVC với Node.js

--- src/server.js
+++ src/server.js
@@ -1,25 +1,15 @@
-const express = require('express');
 require('dotenv').config();
-const path = require('path');
+const express = require('express');
+const configViewEngine = require('./config/viewEngine');
+const webRoutes = require('./routes/web');
 
 const app = express();
 const port = process.env.PORT || 8080;
 const hostname = process.env.HOST_NAME;
 
-//config template engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+configViewEngine(app);
 
-//config static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.get('/duongpc', (req, res) => {
-    res.render('sample.ejs');
-});
+app.use('/', webRoutes);
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`);
