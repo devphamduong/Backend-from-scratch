5b919b560701b4fdedd8e948c1859b16adeb7198
Pham Chu Duong <pduong244@gmail.com>
2023-03-30 09:26:57 +0700

    ENV(Environment Variables)

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+require('dotenv').config();
 const path = require('path');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
+const hostname = process.env.HOST_NAME;
 
 //config template engine
 app.set('views', path.join(__dirname, 'views'));
@@ -16,6 +18,6 @@ app.get('/duongpc', (req, res) => {
     res.render('sample.ejs');
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`);
 });
